5975bbf70eb5e38a1934764fbcaf4f441e393263
Rajiv <0917711@hr.nl>
2019-06-02 22:35:14 +0200

    basic mail function, gotta troubleshoot with the backend team about importing and posting

--- src/backend/flaskr/mailer.py
+++ src/backend/flaskr/mailer.py
@@ -1,1 +1,25 @@
-# TODO Expand stub
+import os
+import smtplib
+
+name = "Rajiv"
+
+EMAIL_ADDRESS = os.environ.get('email_address')
+EMAIL_PASS = os.environ.get('email_pass')
+
+def send_email(subject, msg):
+     
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(EMAIL_ADDRESS, EMAIL_PASS)
+        message = 'subject: {}\n\n{}' .format(subject, msg)
+        server.sendmail(EMAIL_ADDRESS, "y.jethoe@hotmail.com", message)
+        server.quit()
+        print("Email sent!")
+    
+        
+
+subject = (name) 
+msg = "Hope this works ;)"
+send_email(subject, msg)
