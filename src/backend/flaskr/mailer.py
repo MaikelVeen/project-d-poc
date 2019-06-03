752d75b109a16d2aff56e01883d6f49ba26903f3
Rajiv <0917711@hr.nl>
2019-06-03 03:14:27 +0200

    file attachment works, not yet dynamic though

--- src/backend/flaskr/mailer.py
+++ src/backend/flaskr/mailer.py
@@ -1,6 +1,7 @@
 import os
 import smtplib
 from email.message import EmailMessage 
+import imghdr
 
 name = "Rajiv"
 email = "y.jethoe@hotmail.com"
@@ -13,7 +14,15 @@ message = EmailMessage()
 message['Subject'] = name
 message['From'] = "Rajivrocks"
 message['To'] = email
-message.set_content("Hello!")
+message.set_content("Hello! :3")
+
+# this will be dynamic when finished, I provided a test image, feel free to swap it out with your own, but this one is very cute! x)
+with open('2bChibi.png', 'rb') as f:
+        file_data = f.read()
+        file_type = imghdr.what(f.name)
+        file_name = f.name
+
+message.add_attachment(file_data, maintype='image', subtype='file_type', filename=file_name)
 
 def send_email():
         # connecting to gmail smtp mail server
