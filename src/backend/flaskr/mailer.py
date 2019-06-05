818c3ec23a655fdbd42d9700a4fba5ee1e1e2b1e
Maikel Veen <veenmaikel@gmail.com>
2019-06-05 23:22:20 +0200

    Fully integrate the mailer into registration route
    Only the content body should be set to the QR code. Also environment variable configuration should be added to the manual

--- src/backend/flaskr/mailer.py
+++ src/backend/flaskr/mailer.py
@@ -1,29 +1,27 @@
 import os
 import smtplib
-from email.message import EmailMessage 
+from email.message import EmailMessage
 import imghdr
 from flaskr.db import get_db
 
 
 def send_email(email, name, qr_code):
-        # getting OS env's so we don't commit sensitive info to source control
-        EMAIL_ADDRESS = os.environ.get('email_address')
-        EMAIL_PASS = os.environ.get('email_pass')
+    # getting OS env's so we don't commit sensitive info to source control
+    EMAIL_USER = os.environ.get('EMAIL_USER')
+    EMAIL_PASS = os.environ.get('EMAIL_PASSWORD')
 
-        message = EmailMessage()
-        message['Subject'] = name
-        message['From'] = "Rajivrocks"
-        message['To'] = email
-        message.set_content("Hello! :3")
+    message = EmailMessage()
+    message['Subject'] = f"Hi {name}, this is your registration QR Code for lobby checkin"
+    message['To'] = email
+    message.set_content("QR CODE should be here")
 
-        message.add_attachment(file_data, maintype='image', 
-                subtype='file_type',filename=file_name)
+    # message.add_attachment(file_data, maintype='image',
+    #      subtype='file_type',filename=file_name)
 
-        # connecting to gmail smtp mail server
-        server = smtplib.SMTP_SSL('smtp.gmail.com:465')
-        server.login(EMAIL_ADDRESS, EMAIL_PASS)
-        server.send_message(message)
-        server.quit()
-        
-        print("Email sent!")
-        
+    # connecting to gmail smtp mail server
+    server = smtplib.SMTP_SSL('smtp.gmail.com:465')
+    server.login(EMAIL_USER, EMAIL_PASS)
+    server.send_message(message)
+    server.quit()
+
+    print(f"Email sent succesfully to {email}")
--- src/backend/flaskr/register.py
+++ src/backend/flaskr/register.py
@@ -4,8 +4,11 @@ from flask import (
 from flaskr.db import get_db
 from flaskr.mailer import send_email
 from flask_expects_json import expects_json
+from pathlib import Path
+from threading import Thread
 import uuid
 import qrcode
+
 
 bp = Blueprint('register', __name__, url_prefix='/register')
 
@@ -29,20 +32,39 @@ def register_post():
 
     # Generate a unique user id as 32-character hexadecimal string
     user_id = uuid.uuid4().hex
-    user_data.update([('id', user_id)])
 
     db.execute(
         'INSERT INTO user (id, name, email, phone) VALUES (?, ?, ?, ?)',
-        (user_data['id'], user_data['name'],
+        (user_id, user_data['name'],
          user_data['email'], user_data['phone'])
     )
     db.commit()
 
-    # Temporary save the file to show it works
+    # Generate path name for qr image
+    data_folder = Path("images/")
+    path_name = data_folder / f"{user_id}.jpg"
+    Path("images/").mkdir(parents=False, exist_ok=True)
+
+    # Generate the qr code
     qr_img = qrcode.make(user_id)
-    qr_img.save(f"{user_id}.jpg")
-    send_email(user_data['email'], user_data['name'], qr_img)
+    qr_img.save(path_name)
+
+    # Create email thread
+    email_thread = Thread(target=handle_email, args=(
+        user_data['email'], user_data['name'], None,))
+    email_thread.start()
 
     response_dict = dict([('status', False)])
     response = jsonify(response_dict)
     return response, 201
+
+
+def handle_email(email, name, qr):
+    # Send email with qr code to user
+    # TODO add path/image to call
+    try:
+        print(f"Attempting to email {email}")
+        send_email(email, name, qr)
+    except Exception as e:
+        print(f"Encountered exception during email: {e}")
+        print("Silently continiuing")
