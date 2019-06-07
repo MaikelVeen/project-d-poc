01ed426124671297ce07aec2cada2c3a4a08983f
joris v.d. sluis <jjori8@hotmail.com>
2019-06-07 15:37:21 +0200

    Change TimedWebcam to only fetch after response

--- src/frontend/hospitality-app/src/components/TimedWebcamCapture.js
+++ src/frontend/hospitality-app/src/components/TimedWebcamCapture.js
@@ -7,35 +7,46 @@ import indicator from "../assets/indicator.png";
 class TimedWebcamCapture extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { image: "", message: "",  fetch: true };
+    this.state = { image: "", message: "", capture: false };
   }
 
-
+  //set 1 second timerout for webcam start-up
   componentDidMount() {
-   if (this.state.fetch == true){this.capture()}
+    setTimeout(
+      function() {
+        this.setState({ capture: true });
+      }.bind(this),
+      1000
+    );
   }
 
-  componentWillUnmount() {
-    
+  // capture photo every time capture == true
+  componentDidUpdate() {
+    if (this.state.capture == true) {
+      this.capture();
+    }
   }
+  componentWillUnmount() {}
 
   setRef = webcam => {
     this.webcam = webcam;
   };
 
+  //Capture photo and send request, stop calling capture
   capture = () => {
-    
     const imageSource = this.webcam.getScreenshot();
     this.setState(
       {
         image: imageSource,
-        fetch: false
+       capture: false
       },
       () => this.send_request()
     );
   };
 
+  //send post request to backend, after response set capture == true
   send_request = () => {
+    console.log(this.state.image);
     fetch("http://localhost:5000/check/image", {
       method: "POST",
       body: JSON.stringify({
@@ -47,13 +58,13 @@ class TimedWebcamCapture extends React.Component {
       result => {
         this.setState({
           message: result,
-          fetch: true
-
+          capture: true
         });
       },
       error => {
         this.setState({
-          message: error
+          message: error,
+          capture: true
         });
       }
     );
@@ -79,7 +90,6 @@ class TimedWebcamCapture extends React.Component {
                 height="720"
                 width="1280"
               />
-
               <img
                 id="indicator"
                 style={{ zIndex: 2, position: "absolute", top: 1 }}
