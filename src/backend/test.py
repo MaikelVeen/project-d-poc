be62f5a81c66436f4172f29e1a9fe61a42d39724
Rajiv <0917711@hr.nl>
2019-06-10 21:32:51 +0200

    dont mind this push, just some BS

--- src/backend/test.py
+++ src/backend/test.py
@@ -3,6 +3,7 @@ import mailer
 import register
 import recog
 import check_in
+from unittest.mock import patch
 
 class TestMailer(unittest.TestCase):
 
@@ -10,9 +11,20 @@ class TestMailer(unittest.TestCase):
         self.assertIsNone(mailer.send_email("y.jethoe@hotmail.com", "Rajiv", '4f487414cdf44d47ac0378f1fbff9132.jpg'))
 
 class TestRegister(unittest.TestCase):
+
+    def setUp(self):
+        self.cust1 = register('Rajiv', 'y.jethoe@hotmail.com', '0786166143')
+    
+    def tearDown(self):
+        print('teardown\n')
     
     def test_register_post(self):
         # TODO Register_post unittest
+        with patch('register.request.post') as mocked_post:
+            mocked_post.return_value.created = True
+
+            mocked_post.assert_called_with('http://localhost:5000/register/post')
+            self.assertEquals(self.cust1, 'CREATED')
         pass
 
     def test_handle_email(self):
