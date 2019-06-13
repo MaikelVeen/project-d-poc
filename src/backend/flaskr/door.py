2c5a65e95073b2a7badabf4d28f38e42c1a11756
Maikel Veen <veenmaikel@gmail.com>
2019-06-13 16:41:53 +0200

    Add door open route

--- src/backend/flaskr/door.py
+++ src/backend/flaskr/door.py
@@ -1,23 +1,130 @@
+import boto3
 from flask import (
     Blueprint, flash, g, jsonify
 )
 from flaskr.db import get_db
 from flask_expects_json import expects_json
+from pathlib import Path
+import base64
+import uuid
 
 bp = Blueprint('door', __name__, url_prefix='/door')
+THRESHOLD = 0.99
 
+IMAGE_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'image_string': {'type': 'string'},
+        'room': {'type':'number'}
+    },
+    'required': ['room', 'image_string']
+}
 
+# TODO rename route, split function, add documentation
 @bp.route('/numbers', methods=['GET'])
 def check_qr():
     db = get_db()
     rows = db.execute('SELECT roomNumber FROM userRoom').fetchall()
     data = list()
     for row in rows:
-      data.append(row[0])
+        data.append(row[0])
 
-    return jsonify({'result':data}), 200
+    return jsonify({'result': data}), 200
+
 
 @bp.route('/open', methods=['POST'])
-def open():
-    response = 'Joe'
-    return response, 200
+@expects_json(IMAGE_SCHEMA)
+def open_door():
+    image_data = base64.urlsafe_b64decode(g.data['image_string'])
+    target_file_path = _save_attempt(image_data)
+
+    user_id = _get_room_user(g.data['room'])
+    if user_id is None:
+        return None,500
+    
+    source_file_path = _get_source_path(user_id)
+    return _compare(source_file_path, target_file_path), 200
+
+
+def _save_attempt(image):
+    """
+    Save the image that was taken at this login attempt
+
+    Args:
+        image: the webcam capture received in request
+        
+    Returns:
+        Path name of saved image file
+    
+    Raises:
+        ValueError, IOError
+    """
+    data_folder = Path("images/attempt/")
+    Path("images/attempt").mkdir(parents=False, exist_ok=True)
+    path_name = data_folder / (f"{uuid.uuid4().hex}.jpeg")
+
+    with open(path_name, 'wb') as f:
+        f.write(image)
+    
+    return path_name
+
+def _get_room_user(room_number):
+    """
+    Get the id of the user that is linked to a given room number
+
+    Args:
+        room_number: The number of the room
+        
+    Returns:
+        The user_id linked to the room. If no user_id is found return
+
+    """
+    db = get_db()
+    result = db.execute('SELECT user_id FROM userRoom WHERE roomNumber = ?'
+        ,room_number).fetchone()
+
+    if result is None:
+        return None
+    else:
+        return result[0]
+
+def _get_source_path(user_id):
+    """Returns the source file image path based on a user id"""
+    data_folder = Path("images/user_faces/")
+    Path("images/user_faces").mkdir(parents=False, exist_ok=True)
+    path_name = data_folder / (f"{user_id}.jpeg")
+    return path_name
+    
+
+def _compare(source_file_path, target_file_path):
+    """
+    Compare the face of attempt to the face saved at checkin
+    Essentialy a wrapper for boto3 compare_faces function
+
+    Args:
+        source_file_path: path to source file
+        target_file_path: path to image current attempt
+        
+    Returns:
+        True if face's similarity is above threshold
+        False if face's similarity is not above threshold
+
+    """
+    amazon_client = boto3.client('rekognition')
+
+    image_source = open(source_file_path, 'rb')
+    image_target = open(target_file_path, 'rb')
+
+    try:
+        response = amazon_client.compare_faces(SimilarityThreshold=0, SourceImage={
+                                               'Bytes': image_source.read()}, TargetImage={'Bytes': image_target.read()})
+    except:
+        return False
+
+    if len(response['FaceMatches']) == 0:
+        return False
+    
+    similarity = response['FaceMatches'][0]['Similarity']
+    print(similarity)
+    return True
+    
--- src/backend/flaskr/recog.py
+++ src/backend/flaskr/recog.py
@@ -8,31 +8,32 @@ from flask_expects_json import expects_json
 bp = Blueprint('recog', __name__, url_prefix='/recog')
 
 # TODO Expand Stub
-# 
+#
 # Probleem met importen van flaskr bij het uitvoeren van het script
 # Zodra je de recog.py buiten de map flaskr plaatst werkt het wel: backend/recog.py i.p.v. backend/flaskr/recog.py
 
+
 def amazon(source_file, target_file):
 
-	client = boto3.client('rekognition')
+    client = boto3.client('rekognition')
 
-	image_source = open(source_file, 'rb')
-	image_target = open(target_file, 'rb')
+    image_source = open(source_file, 'rb')
+    image_target = open(target_file, 'rb')
 
-	try:
-		response = client.compare_faces(SimilarityThreshold=0,
-									SourceImage={'Bytes': image_source.read()},
-									TargetImage={'Bytes': image_target.read()})
-	except:
-		return "No Face Detected"
+    try:
+        response = client.compare_faces(SimilarityThreshold=0,
+                                        SourceImage={
+                                            'Bytes': image_source.read()},
+                                        TargetImage={'Bytes': image_target.read()})
+    except:
+        return "No Face Detected"
 
-	for faceMatch in response['FaceMatches']:
+    for faceMatch in response['FaceMatches']:
 
-		confidence = str(faceMatch['Similarity'])
+        confidence = str(faceMatch['Similarity'])
 
+    image_source.close()
+    image_target.close()
 
-	image_source.close()
-	image_target.close()
-
-	print(confidence)
-	return confidence
+    print(confidence)
+    return confidence
