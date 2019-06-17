4d9da3f2dc1fb26d79e65642e33c3366bd1ba8e6
Rajiv <0917711@hr.nl>
2019-06-17 15:14:54 +0200

    Unit-Testing

---
+++ src/backend/flaskr/test.py
@@ -0,0 +1,42 @@
+import unittest
+import flaskr.mailer
+import flaskr.register
+import flaskr.recog
+import flaskr.check_in
+
+
+class TestMailer(unittest.TestCase):
+
+    def test_send_email(self):
+        # The jpeg is a created QR code this is one that I created for the purpose of this test so if you would run this you would first need to create a QRCode and paste the filename in here. also
+        # needs to be in the same filepath as the test.py file
+        self.assertIsNone(mailer.send_email("y.jethoe@hotmail.com", "Rajiv", '4f487414cdf44d47ac0378f1fbff9132.jpg'))
+
+class TestRegister(unittest.TestCase):
+    
+    def test_register_post(self):
+        # TODO Register_post unittest\
+        pass
+        
+
+    def test_handle_email(self):
+        # The jpeg is a created QR code this is one that I created for the purpose of this test so if you would run this you would first need to create a QRCode and paste the filename in here. also
+        # needs to be in the same filepath as the test.py file
+        self.assertIsNone(register.send_email("y.jethoe@hotmail.com", "Test Person", "4f487414cdf44d47ac0378f1fbff9132.jpg"))
+
+class TestRecog(unittest.TestCase):
+    def test_amazon(self,):
+        # TODO amazon unittest
+        pass
+
+class TestCheck_in(unittest.TestCase):
+    def test_check_qr(self):
+        # TODO check_qr unittest
+        pass
+
+    def test_send_image(self):
+        # TODO send_image unittest
+        pass
+
+if __name__ == '__main__':
+    unittest.main() 
